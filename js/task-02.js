087fb234b02e238b2a0062f393ae1b0bcde3dee2
Hrytsenia Nazar <hrytseniaaa@gmail.com>
2022-07-29 03:35:37 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = [];
+
+for (let ingredient of ingredients) {
+
+  const ingredientItem = document.createElement('li');
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add('item');
+  // console.log(ingredientItem);
+
+  list.push(ingredientItem);
+};
+// console.log(...list);
+
+
+const ingredientsEl = document.querySelector('ul#ingredients');
+ingredientsEl.append(...list);
+console.log(ingredientsEl);
+
+// const ingredient = document.createElement('li')
+// ingredient.textContent = 'Potatoes';
+// ingredient.classList.add('item')
+// console.log(ingredient);
