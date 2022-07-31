a55a53e035575d2770c5ad1aafd9eb0639343d1f
Hrytsenia Nazar <hrytseniaaa@gmail.com>
2022-08-01 00:25:50 +0300

    таск 5

--- js/task-04.js
+++ js/task-04.js
@@ -8,7 +8,7 @@ const refs  = {
 
     minus : document.querySelector('[data-action="decrement"]'),
     plus : document.querySelector('[data-action="increment"]'),
-    value : document.querySelector('#value')
+    currentValue : document.querySelector('#value')
 
 };
 
@@ -17,14 +17,14 @@ const refs  = {
 
 refs.minus.addEventListener('click', () => {
     counterValue -= 1;
-    refs.value.textContent = counterValue;
+    refs.currentValue.textContent = counterValue;
 });
 
 refs.plus.addEventListener ('click', plusToValue)
 
 function plusToValue() {
     counterValue += 1;
-    refs.value.textContent = counterValue;
+    refs.currentValue.textContent = counterValue;
 
     // 
 }
